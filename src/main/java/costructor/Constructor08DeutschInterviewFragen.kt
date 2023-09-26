3f7cf804435f366f1cb4a9d044973ba9c0065c72
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2023-09-26 23:49:57 +0200

    fourteenth1

---
+++ src/main/java/costructor/Constructor08DeutschInterviewFragen.kt
@@ -0,0 +1,48 @@
+package costructor
+
+class Constructor08DeutschInterviewFragen {
+    /*
+1. Was ist ein Konstruktor in Java?
+
+Ein Konstruktor ist eine spezielle Methode in einer Java-Klasse, die aufgerufen wird,
+wenn ein Objekt dieser Klasse erstellt wird.
+Er initialisiert die Eigenschaften des Objekts und führt andere Aufgaben aus,
+die beim Erstellen des Objekts erforderlich sind.
+
+2. Welche Arten von Konstruktoren gibt es in Java?
+
+In Java gibt es zwei Hauptarten von Konstruktoren:
+Default-Konstruktor: Wenn Sie keinen Konstruktor in Ihrer Klasse definieren,
+wird automatisch ein Standardkonstruktor ohne Parameter erstellt.
+Benutzerdefinierter Konstruktor: Sie können eigene Konstruktoren erstellen,
+um Objekte mit bestimmten Werten zu initialisieren.
+
+3. Wie unterscheidet sich ein Konstruktor von einer normalen Methode?
+
+Konstruktoren haben denselben Namen wie die Klasse selbst, während Methoden unterschiedliche Namen haben können.
+Konstruktoren werden automatisch aufgerufen, wenn ein Objekt erstellt wird,
+während Methoden explizit aufgerufen werden müssen.
+Konstruktoren haben keinen Rückgabetyp, nicht einmal void.
+
+5. Können Sie mehrere Konstruktoren in einer Klasse haben?
+
+Ja, Sie können mehrere Konstruktoren in einer Klasse haben,
+solange sie sich in der Anzahl oder Art der Parameter unterscheiden.
+Dies wird als Überladen von Konstruktoren bezeichnet.
+
+7. Was ist ein Parameterkonstruktor?
+
+Ein Parameterkonstruktor ist ein benutzerdefinierter Konstruktor, der Parameter akzeptiert,
+um die Objekteigenschaften während der Initialisierung festzulegen.
+
+9. Was ist der Zweck eines Standardkonstruktors in Java?
+
+Der Standardkonstruktor dient dazu,ein Objekt zu erstellen und dessen Eigenschaften mit Standardwerten zu initialisieren,
+wenn kein benutzerdefinierter Konstruktor vorhanden ist.
+
+
+
+
+     */
+
+}
---
+++ src/main/java/costructor/Constructor08EnglishInterviewQuestions.java
@@ -0,0 +1,38 @@
+package costructor;
+
+public class Constructor08EnglishInterviewQuestions {
+    /*
+1. What is a constructor in Java?
+
+A constructor is a special method in a Java class that is called when an object of that class is created.
+It initializes the properties of the object and performs other tasks necessary during object creation.
+
+2. What types of constructors are there in Java?
+
+In Java, there are two main types of constructors:
+Default Constructor: When you do not define a constructor in your class,
+a default constructor with no parameters is automatically created.
+Parameterized Constructor: You can create custom constructors to initialize objects with specific values.
+
+3. How does a constructor differ from a regular method?
+
+Constructors have the same name as the class itself, whereas methods can have different names.
+Constructors are automatically called when an object is created, while methods must be explicitly called.
+Constructors do not have a return type, not even void.
+
+5. Can you have multiple constructors in a class?
+
+Yes, you can have multiple constructors in a class as long as they differ in the number or type of parameters.
+This is known as constructor overloading.
+
+7. What is a parameterized constructor?
+
+A parameterized constructor is a custom constructor that accepts parameters to set object properties during initialization.
+
+9. What is the purpose of a default constructor in Java?
+
+The default constructor is used to create an object and
+initialize its properties with default values when no custom constructor is present.
+
+     */
+}
