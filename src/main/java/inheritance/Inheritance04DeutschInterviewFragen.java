6aca4114ae3be2682ff19e45472a1bb42403d1df
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2023-10-11 14:07:05 +0200

    eighteen1

---
+++ src/main/java/inheritance/Inheritance04DeutschInterviewFragen.java
@@ -0,0 +1,42 @@
+package inheritance;
+
+public class Inheritance04DeutschInterviewFragen {
+    /*
+
+ Frage 1: Was ist Vererbung in Java und warum ist sie wichtig?
+
+Antwort: Vererbung ist ein Konzept in der objektorientierten Programmierung,
+bei dem eine Klasse (die Subklasse oder abgeleitete Klasse) die Eigenschaften
+und Methoden einer anderen Klasse (die Basisklasse oder Superklasse) erbt.
+Dies ermöglicht die Wiederverwendung von Code und die Erstellung von Hierarchien von Klassen.
+Vererbung ist wichtig, da sie die Grundlage für die Erstellung von Beziehungen zwischen Klassen bildet
+und die Strukturierung des Codes erleichtert.
+
+Frage 2: Wie wird Vererbung in Java implementiert?
+
+Antwort: In Java wird Vererbung mithilfe des Schlüsselworts "extends" realisiert.
+Die Subklasse erbt die Eigenschaften und Methoden der Superklasse.
+
+Frage 4: Was ist die "Super"-Klasse und wie wird sie in Java verwendet?
+
+Antwort: Die "Super"-Klasse ist die Basisklasse, von der eine Subklasse erbt.
+Sie wird in Java mithilfe des Schlüsselworts super verwendet,
+um auf die Eigenschaften und Methoden der Superklasse zuzugreifen.
+
+Frage 5: Welche Arten von Vererbung gibt es in Java?
+
+Antwort: In Java gibt es zwei Hauptarten von Vererbung:
+
+Single Inheritance: Eine Klasse erbt von einer einzigen Superklasse.
+
+Frage 6: Was ist der Unterschied zwischen "is-a" und "has-a" Beziehungen in Bezug auf Vererbung?
+
+Antwort: In der Vererbung beschreibt die "is-a" Beziehung eine Beziehung,
+in der eine Subklasse eine spezialisierte Form der Superklasse ist.
+Die "has-a" Beziehung bezieht sich auf die Verwendung einer anderen Klasse als Teil eines Objekts
+
+
+
+
+     */
+}
---
+++ src/main/java/inheritance/Inheritance04EnglishQuestions.java
@@ -0,0 +1,42 @@
+package inheritance;
+
+public class Inheritance04EnglishQuestions {
+
+/*
+
+Question 1: What is inheritance in Java, and why is it important?
+
+Answer: Inheritance is a concept in object-oriented programming where
+a class (the subclass or derived class) inherits the properties and methods of another class (the base class or superclass).
+It allows for code reuse and the creation of class hierarchies.
+Inheritance is important because it forms the basis for establishing relationships between classes and facilitates code structuring.
+
+Question 2: How is inheritance implemented in Java?
+
+Answer: In Java, inheritance is implemented using the "extends" keyword.
+The subclass inherits the properties and methods of the superclass.
+
+Question 4: What is the "super" class, and how is it used in Java?
+
+Answer: The "super" class is the base class from which a subclass inherits.
+In Java, it is used with the super keyword to access properties and methods of the superclass.
+This is useful when there are properties or methods with the same name in both the subclass and the superclass,
+ensuring you access the ones defined in the superclass.
+
+Question 5: What are the types of inheritance in Java?
+
+Answer: In Java, there are two main types of inheritance:
+
+Single Inheritance: A class inherits from a single superclass.
+
+Question 6: What is the difference between "is-a" and "has-a" relationships in inheritance?
+
+Answer: In the context of inheritance, the "is-a" relationship describes a relationship where
+a subclass is a specialized form of the superclass.
+The "has-a" relationship pertains to using another class as part of an object.
+
+
+
+ */
+
+}
