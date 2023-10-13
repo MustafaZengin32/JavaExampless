97e9428f8c6ac4ab31d582eed1dab263ebfcbc16
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2023-10-14 00:55:11 +0200

    nineteen

---
+++ src/main/java/abstractclass/Abstract01IdariPersonelChildClass.java
@@ -0,0 +1,12 @@
+package abstractclass;
+
+public class Abstract01IdariPersonelChildClass extends Abstract01IdariPersonel {
+
+    public static void main(String[] args) {
+
+        Abstract01IdariPersonelChildClass obj1 =new Abstract01IdariPersonelChildClass();
+        obj1.maasHesapla(); // idari personel maasi 4000 tl
+
+
+    }
+}
