ca0d1a68671248df6766a1a53f12a014a6e8a7ad
MustafaZengin32 <120048898+MustafaZengin32@users.noreply.github.com>
2023-10-28 12:31:58 +0200

    twenty one

---
+++ src/main/java/set/Set07DeutschInterviewFragen.java
@@ -0,0 +1,51 @@
+package set;
+
+public class Set07DeutschInterviewFragen {
+    /*
+
+1. Was ist ein Set in Java?
+
+Ein Set ist ein Interface in Java, das Teil des Java Collections Frameworks ist.
+Es wird verwendet, um eine Sammlung von Elementen zu speichern,
+erlaubt jedoch im Gegensatz zu einer List keine Duplikate.
+
+2. Welche sind die wichtigsten Implementierungen des Set-Interfaces in Java?
+
+Die Hauptimplementierungen des Set-Interfaces in Java sind:
+HashSet: Es verwendet eine Hashtabelle zur Speicherung und garantiert keine bestimmte Reihenfolge der Elemente.
+LinkedHashSet: Es behält die Reihenfolge der Elemente bei, wie sie hinzugefügt wurden.
+TreeSet: Es speichert Elemente in sortierter Reihenfolge
+(natürliche Reihenfolge oder mit einem benutzerdefinierten Comparator).
+
+3. Wie stellen Sie sicher, dass ein Set keine Duplikate zulässt?
+
+Das Set-Interface selbst erlaubt keine Duplikate.
+Die Einzigartigkeit wird durch die hashCode- und equals-Methoden der Elemente sichergestellt.
+
+5. Wie durchlaufen Sie die Elemente in einem Set?
+
+Sie können einen Iterator oder die erweiterte for-Schleife
+(for-each-Schleife) verwenden, um die Elemente in einem Set zu durchlaufen.
+
+6. Was ist der Unterschied zwischen den Methoden add() und addAll() zum Hinzufügen von Elementen zu einem Set?
+
+Die Methode add() wird verwendet, um ein einzelnes Element zu einem Set hinzuzufügen.
+Die Methode addAll() wird verwendet, um alle Elemente aus einer anderen Sammlung zu einem Set hinzuzufügen.
+
+7. Wie prüfen Sie, ob ein bestimmtes Element in einem Set existiert?
+
+Sie können die Methode contains(Object obj) verwenden, um zu überprüfen,
+ob ein bestimmtes Element in einem Set vorhanden ist.
+
+8. Kann ein null-Wert zu einem Set hinzugefügt werden?
+
+Ja, die meisten Implementierungen von Set in Java erlauben das Hinzufügen eines null-Werts.
+
+10. Was ist der Unterschied zwischen einem Set und einer List?
+- Ein Set erlaubt keine Duplikate und garantiert keine bestimmte Reihenfolge,
+während eine List Duplikate erlaubt und die Reihenfolge beibehält, in der die Elemente hinzugefügt wurden.
+
+
+
+     */
+}
---
+++ src/main/java/set/Set07EnglishQuestions.java
@@ -0,0 +1,54 @@
+package set;
+
+public class Set07EnglishQuestions {
+
+    /*
+
+1. What is a Set in Java?
+
+A Set is an interface in Java that is part of the Java Collections Framework.
+It is used to store a collection of elements, but unlike a List, it does not allow duplicate elements.
+2. What are the main implementations of the Set interface in Java?
+
+The primary implementations of the Set interface in Java are:
+HashSet: It uses a hash table for storage and does not guarantee the order of elements.
+LinkedHashSet: It maintains the order of elements as they were added.
+TreeSet: It stores elements in a sorted (natural order or with a custom Comparator) manner.
+3. How do you ensure that a Set does not allow duplicate elements?
+
+The Set interface inherently does not allow duplicate elements.
+It relies on the hashCode and equals methods to ensure uniqueness.
+
+4. What is the difference between HashSet, LinkedHashSet, and TreeSet?
+
+HashSet uses a hash table for storage and does not guarantee the order of elements.
+LinkedHashSet maintains the order of elements as they were added.
+TreeSet stores elements in a sorted manner (natural order or custom order based on a Comparator).
+5. How do you iterate through elements in a Set?
+
+You can use an iterator or the enhanced for loop (for-each loop) to iterate through the elements in a Set.
+
+6. What is the difference between add() and addAll() methods for adding elements to a Set?
+
+The add() method is used to add a single element to a Set.
+The addAll() method is used to add all elements from another collection to a Set.
+
+7. How do you check if a particular element exists in a Set?
+
+You can use the contains(Object obj) method to check if a specific element exists in a Set.
+
+8. Can a null value be added to a Set?
+
+Yes, most Set implementations in Java allow a null value to be added.
+
+9. Explain the use of the Set interface in Java applications.
+
+The Set interface is commonly used in situations where you need to store a collection of unique elements,
+such as maintaining a list of unique items, removing duplicates from a collection, or checking for the existence of items.
+
+10. What is the difference between a Set and a List?
+- A Set does not allow duplicate elements and does not guarantee any specific order,
+ while a List allows duplicate elements and maintains the order in which elements were added.
+
+     */
+}
